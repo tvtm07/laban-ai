cc6aed963e6eebb807bffec87e08cff55dd060eb
tvtm07 <tranvuthuyminh030407@gmail.com>
2026-03-22 09:10:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 # Tạm thời bạn dán API Key vào đây để chạy thử trên máy.
 # --- CẤU HÌNH API ---
 # Tạm thời bạn dán API Key vào đây để chạy thử trên máy.
-API_KEY = "DÁN_API_KEY_CỦA_BẠN_VÀO_ĐÂY"
+API_KEY = "AIzaSyA_R4xk7_YWbQ4RBtst0O5-subg13R1CeY"
 genai.configure(api_key=API_KEY)
 
 # Tuyệt chiêu: Tự động quét và chọn mô hình AI phù hợp nhất
